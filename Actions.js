17ad5f1686846c4050452d0af7fbbb3ba85b283a
mmrahneshin <mmrahneshin@gmail.com>
2022-07-06 02:26:27 +0430

    state and actions class

---
+++ Actions.js
@@ -0,0 +1,12 @@
+class Actions {
+
+    constructor(name, pos_preconditions, pos_preconditions, addList, deleteList) {
+
+        this.name = name;
+        this.pos_preconditions = pos_preconditions;
+        this.addList = addList;
+        this.deleteList = deleteList;
+
+    }
+
+}
---
+++ State.js
@@ -0,0 +1,9 @@
+class State {
+
+    constructor(parent, action, pos_literals, neg_literals) {
+        this.parent = parent;
+        this.action = action;
+        this.pos_literals = pos_literals;
+        this.neg_literals = neg_literals;
+    }
+}
