11ffb1d26045057ccd2cd06ffbd1eb00eb6756b3
mmrahneshin <mmrahneshin@gmail.com>
2022-07-06 02:33:00 +0430

    write all the classes that may be needed

--- Action.js
+++ Action.js
@@ -1,9 +1,10 @@
-class Actions {
+class Action {
 
-    constructor(name, pos_preconditions, pos_preconditions, addList, deleteList) {
+    constructor(name, pos_preconditions, neg_preconditions, addList, deleteList) {
 
         this.name = name;
         this.pos_preconditions = pos_preconditions;
+        this.neg_preconditions = neg_preconditions;
         this.addList = addList;
         this.deleteList = deleteList;
 
